035dee3ffc507fa9a5d672a877bd3ac83a390734
choiwooseok <wooseok777777@gmail.com>
2021-07-10 03:20:19 +0900

    Algorithm

--- Gold5/AC.py
+++ Gold5/AC.py
@@ -1,51 +1,42 @@
-import sys
-input =sys.stdin.readline
-
-def go(string,array):
-    Order = True
-    left = 0
-    right = len(array)-1
-    for op in string:
-        if op=="R":
-            Order = not Order
-        elif op=="D":
-            if Order:
-                left+=1
-            else:
-                right-=1
-    answer = ""
-    ret = []
-    print(Order,left,right)
-    print(type(array))
-    if left<=right and Order:
-        for v in array[left:right+1]:
-            ret.append(str(v)+",")
-        ret.pop()
-        ret.insert(0,"[")
-        ret.insert(-1,"]")
-        answer += ''.join(ret)
-        print(answer)
-    elif left<=right and not Order:
-        for v in array[left:right + 1]:
-            ret.append(str(v) + ",")
-        ret.reverse()
-        ret.pop()
-        ret.insert(0,"[")
-        ret.insert(-1,"]")
-        answer += ''.join(ret)
-        print(answer)
-    else:
-        answer += "error"
-    return answer
-
+import sys,re
+from collections import deque
 t = int(input())
 for _ in range(t):
-    query = input().strip()
+    query = list(input().strip())
     n = int(input())
-    a = eval(input())
-    if n != 0:
-        ans = go(query,a)
-        print(ans)
+    input_ = sys.stdin.readline()
+    l = re.split("\[|\]|,|\n", input_)
+    l = deque(l[1:-2])
+
+    #0처리
+    Error = False
+    if n==0:
+        for v in query:
+            if v=="D":
+                Error = True
+        if Error:
+            print("error")
+        else:
+            print("[]")
+        continue
+    Same = True
+    Fail = False
+    for v in query:
+        if v=='D':
+            if not l:
+                Fail = True
+                break
+
+            if Same:
+                l.popleft()
+            if not Same:
+                l.pop()
+        else:
+            Same = not Same
+    if Fail:
+        print("error")
     else:
-        if "D" in query:print("error")
-        else :print("[]")
+        if not Same:l.reverse()
+        print("[",end='')
+        print(",".join(l),end='')
+        print("]")
