5190d97cfe05fb737524c0e9ed7ddc1a03b80f08
Wooseok <64106149+wooseoking@users.noreply.github.com>
2024-01-31 18:16:18 +0900

    Update ［PCCP 기출문제］ 4번 ／ 수레 움직이기.py

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
@@ -1,5 +1,3 @@
-import copy
-
 dy = [-1,1,0,0]
 dx = [0,0,-1,1]
 
@@ -31,16 +29,16 @@ def solution(maze):
     
     
     #bfs
-    rv = [[False]*m for _ in range(n)]
-    bv = [[False]*m for _ in range(n)]
-    rv[sry][srx] = True
-    bv[sby][sbx] = True
+    
+    # 방문처리 메모리 절약 비트마스킹 
+    rv = 1 << (sry * n + srx)
+    bv = 1 << (sby * n + sbx)
     q = []
     q.append([sry,srx,sby,sbx,0,rv,bv])
     
     while q:
         ry,rx,by,bx,cnt,rv,bv = q.pop(0)
-        print('depth : ',cnt, ' --> ',ry,rx,by,bx)
+        
         #성공조건
         if ry == ery and rx == erx and by == eby and bx == ebx: return cnt
         
@@ -49,34 +47,33 @@ def solution(maze):
             
             for k in range(4):
                 nry,nrx,nby,nbx = ry,rx,by,bx
-                nrv,nbv = copy.deepcopy(rv),copy.deepcopy(bv)
-                
                 nby,nbx = by + dy[k] , bx + dx[k]
                 
                 if (not inside(nry,nrx,n,m)) or (not inside(nby,nbx,n,m)):continue
                 if ry == nby and rx == nbx and nry == by and nrx == bx:continue
-                if nbv[nby][nbx]:continue               
+                if bv & (1 << nby * n + nbx):continue               
                 if maze[nry][nrx] == WALL or maze[nby][nbx] == WALL:continue
-                
                 if nry == nby and nrx == nbx:continue
-                nrv[nry][nrx] = True
-                nbv[nby][nbx] = True
+                
+                nrv = rv | (1 << nry * n + nrx)
+                nbv = bv | (1 << nby * n + nbx)
                 
                 q.append([nry,nrx,nby,nbx,cnt + 1,nrv,nbv])
         #파랑 끝났다면
         elif by == eby and bx == ebx: 
             for k in range(4):
                 nry,nrx,nby,nbx = ry,rx,by,bx
-                nrv,nbv = copy.deepcopy(rv),copy.deepcopy(bv)
-                
                 nry,nrx = ry + dy[k] , rx + dx[k]
+                
                 if (not inside(nry,nrx,n,m)) or (not inside(nby,nbx,n,m)):continue
                 if ry == nby and rx == nbx and nry == by and nrx == bx:continue
-                if nrv[nry][nrx]:continue
+                if rv & (1 << nry * n + nrx):continue
                 if maze[nry][nrx] == WALL or maze[nby][nbx] == WALL:continue
                 if nry == nby and nrx == nbx:continue
-                nrv[nry][nrx] = True
-                nbv[nby][nbx] = True
+                
+                nrv = rv | (1 << nry * n + nrx)
+                nbv = bv | (1 << nby * n + nbx)
+                
                 q.append([nry,nrx,nby,nbx,cnt + 1,nrv,nbv])
 
         #둘다 안끝남
@@ -84,18 +81,20 @@ def solution(maze):
             for rk in range(4):
                 for bk in range(4):
                     nry,nrx,nby,nbx = ry,rx,by,bx
-                    nrv,nbv = copy.deepcopy(rv),copy.deepcopy(bv)
                     
                     nry,nrx = ry + dy[rk] , rx + dx[rk]
                     nby,nbx = by + dy[bk] , bx + dx[bk]
+                    
                     if (not inside(nry,nrx,n,m)) or (not inside(nby,nbx,n,m)):continue
                     if ry == nby and rx == nbx and nry == by and nrx == bx:continue
-                    if nrv[nry][nrx]:continue
-                    if nbv[nby][nbx]:continue
+                    if rv & (1 << nry * n + nrx):continue
+                    if bv & (1 << nby * n + nbx):continue 
                     if maze[nry][nrx] == WALL or maze[nby][nbx] == WALL:continue
                     if nry == nby and nrx == nbx:continue
-                    nrv[nry][nrx] = True
-                    nbv[nby][nbx] = True
+                    
+                    nrv = rv | 1 << nry * n + nrx
+                    nbv = bv | 1 << nby * n + nbx
+                    
                     q.append([nry,nrx,nby,nbx,cnt + 1,nrv,nbv])
 
                 
